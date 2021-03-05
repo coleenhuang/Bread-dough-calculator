c4307efbfc3a0c314f149c76abcd1a22d83eb5a9
Coleen Huang <coleenhuang@gmail.com>
2021-03-05 09:46:13 -0800

    added src folder

--- sample/sample.js
+++ sample/sample.js
@@ -3,5 +3,3 @@ Add a new section of preferments after clicking the button
 Add field for flour/liquid/another ingredient after clicking the button
 Add functions for processing and calculating the submitted forms
 */
-
-document.querySelector(".add-ingredient")
