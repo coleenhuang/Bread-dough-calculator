b635ddaae335021336ceb60ecac66272fc888191
Coleen Huang <coleenhuang@gmail.com>
2021-03-05 14:57:38 -0800

    added gitignore file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Interesting')
+//create heading
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+const app = document.querySelector('#root')
+app.append(heading)
